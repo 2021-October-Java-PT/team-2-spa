28467be2534a057c180b66c181d8dba342c2b017
George Avent <gaventjr@gmail.com>
2022-01-27 16:21:39 -0500

    pagestart

--- client-side/js/index.js
+++ client-side/js/index.js
@@ -1,1 +1,25 @@
-console.log('Client Side is wired up!');
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+const app = document.querySelector("#app");
+
+pageStart();
+
+function pageStart() {
+  about();
+  contact();
+}
+
+function about() {
+  const aboutElem = document.querySelector();
+  aboutElem.addEventListener("click", () => {
+    app.innerHTML = About();
+  });
+}
+
+function navContact() {
+  const contactElem = document.querySelector();
+  contactElem.addEventListener("click", () => {
+    app.innerHTML = Contact();
+  });
+}
