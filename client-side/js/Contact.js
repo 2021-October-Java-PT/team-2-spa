0e09d3ab395616aa118f717f9ce83bbaf4677595
DevDad1 <ryan@ryanwebster.dev>
2022-01-27 19:21:17 -0500

    added asset folder, added an image to use on the contact branch, created contact.js

---
+++ client-side/js/Contact.js
@@ -0,0 +1,15 @@
+import contactImg from '../../assets/contact.jpg';
+
+export default function Contact() {
+    return `
+    <div class="page__container">
+      <div>
+          <img class='page-image' src='${contactImg}' alt='JAC'/>
+          <h2>Contact Us</h2>
+          <article>
+            <a class='contact-link' href='https://github.com/cljenkinsjr/jackr-enterprises-site'>Go To JAC Enterprises</a>
+          </article>
+      </div>
+    </div>
+      `;
+  }
