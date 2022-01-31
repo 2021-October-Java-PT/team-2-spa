58e7ac9fda66d16c7ce1dbae0d71fe674a715d15
DevDad1 <ryan@ryanwebster.dev>
2022-01-31 17:55:00 -0500

    getRequest

--- client-side/js/apiHelpers.js
+++ client-side/js/apiHelpers.js
@@ -1,3 +1,10 @@
+function getRequest(location, callback) {
+    fetch(location)
+      .then((response) => response.json())
+      .then((jsonData) => callback(jsonData))
+      .catch((err) => console.log(err));
+  }
+
 function postRequest(location, requestBody, callback) {
     fetch(location, {
       method: 'POST',
