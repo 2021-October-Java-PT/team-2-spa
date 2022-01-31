45b43d03057546d90b981c0041ec7e789ddb8379
DevDad1 <ryan@ryanwebster.dev>
2022-01-31 17:45:32 -0500

    putRequest

--- client-side/js/apiHelpers.js
+++ client-side/js/apiHelpers.js
@@ -10,3 +10,16 @@ function postRequest(location, requestBody, callback) {
       .then((jsonData) => callback(jsonData))
       .catch((err) => console.log(err));
   }
+
+function putRequest(location, requestBody, callback) {
+    fetch(location, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestBody),
+    })
+      .then((response) => response.json())
+      .then((jsonData) => callback(jsonData))
+      .catch((err) => console.log(err));
+  }
