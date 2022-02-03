2b21e0f5e4833ea8d7c9cb7352a5b25ef83cd866
TreyBrowder <tbrowder7@ymail.com>
2022-02-03 16:47:16 -0500

    finished header, footer, and added into indexJS

--- client-side/js/Components/Header.js
+++ client-side/js/Components/Header.js
@@ -1,12 +1,13 @@
 export default function Header() {
-    return `
-      <h1>JAC Enterpries - SPA </h1>
-      <div class="nav-list-container">
-        <ul class='nav-list'>
-            <li class='nav-list__home'>Home</li>
-            <li class='nav-list__contact'>About Us</li>
-            <li class='nav-list__contact'>Contact Us</li>
-        </ul>
-      </div>
-      `;
-  }
+  return `
+    <h1>JAC Enterpries - SPA </h1>
+    <div class="nav-list-container">
+      <ul class='nav-list'>
+          <li class='nav-list__home'>Home</li>
+          <li class='nav-list__ninjaTurtles'>Ninja Turtles</li>
+          <li class='nav-list__aboutUs'>About Us</li>
+          <li class='nav-list__contactUs'>Contact Us</li>
+      </ul>
+    </div>
+    `;
+}
